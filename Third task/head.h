b13ae691d1ee82bb23c6e7de828de2ab6b9f7178
John <jonnyuz99@gmail.com>
2019-03-16 22:54:24 +0300

    definitons

--- Third task/head.h
+++ Third task/head.h
@@ -11,7 +11,7 @@
 #define RADIUS 5
 
 void CreatePositive(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::CircleShape>&);
-void CreateNegative(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::CircleShape>&);
+void CreateNegative(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::CircleShape>&); 
 void MakeForce();
 void CreateLine();
 
