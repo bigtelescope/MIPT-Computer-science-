7cb5d4121ce2dba2cc903a394aaa064dbb508097
John <jonnyuz99@gmail.com>
2019-03-16 22:53:07 +0300

    main

--- Third task/main.cpp
+++ Third task/main.cpp
@@ -1,6 +1,62 @@
-#include <iostream>
+#include "header.h"
+
+using namespace sf;
 
 int main()
 {
-  return 0;
+    RenderWindow window(VideoMode(HIGHT, LENGHT), "Charges & Lines");
+
+    std::vector<Vertex> forward_points;
+    std::vector<Vertex> backward_points;
+
+    std::vector<CircleShape> positive_charges;
+    std::vector<CircleShape> negative_charges;
+
+    std::vector<Vertex> centres_positive;
+    std::vector<Vertex> centres_negative;
+
+    int space_touch = 0;
+
+
+    while(window.isOpen())
+    {
+        Event event;
+
+        while(window.pollEvent(event))
+        {
+            switch(event.type)
+            {
+                case Event::Closed:
+                    window.close();
+                    break;
+
+
+                case Event::KeyPressed:
+
+                    if(event.key.code == Keyboard::R)
+                        CreatePositive(window, centres_positive, positive_charges);
+
+                    if(event.key.code == Keyboard::N)
+                        CreateNegative(window, centres_negative, negative_charges);
+                    break;
+                        
+
+                case Event::MouseButtonPressed:
+
+                    if(event.mouseButton.button  == Mouse::Left)
+                        CreateLine();
+                    break;
+            }
+        }
+        window.clear();
+
+        for(auto it : positive_charges)
+            window.draw(it);
+        for(auto it : negative_charges)
+            window.draw(it);
+        
+        window.display();
+    }
+
+    return 0;
 }
