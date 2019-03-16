5b290522f8409066d896858eba18feac9be02437
John <jonnyuz99@gmail.com>
2019-03-16 22:53:19 +0300

    main

---
+++ Third task/head.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_H_
+#define HEADER_H_
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
+#define HIGHT 1200
+#define LENGHT 800
+
+#define RADIUS 5
+
+void CreatePositive(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::CircleShape>&);
+void CreateNegative(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::CircleShape>&);
+void MakeForce();
+void CreateLine();
+
+#endif
+
