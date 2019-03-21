796a4fcd493276404fe17f8cd94572ddba1a57ab
John <jonnyuz99@gmail.com>
2019-03-22 00:29:25 +0300

    almost nothing

--- Third task/head.h
+++ Third task/head.h
@@ -4,16 +4,17 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
-#define HIGHT 1200
-#define LENGHT 800
+#define HIGHT 	1200
+#define LENGHT	800
 
-#define RADIUS 5
+#define RADIUS 	5
+#define STEP	2000
 
 void CreatePositive(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::CircleShape>&);
-void CreateNegative(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::CircleShape>&); 
-void MakeForce();
+void CreateNegative(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::CircleShape>&);
+void CalculateForce(sf::RenderWindow&, std::vector<sf::Vertex>&, std::vector<sf::Vertex>&, std::vector<sf::Vertex>&, std::vector<sf::Vertex>&);
 void CreateLine();
 
 #endif
-
