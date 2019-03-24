f46c2d2d881bef4173488c9bfa300fa596f7c600
John <jonnyuz99@gmail.com>
2019-03-24 20:12:18 +0300

    builds lines

--- Third task/main.cpp
+++ Third task/main.cpp
@@ -1,10 +1,10 @@
-#include "header.h"
+#include "head.h"
 
 using namespace sf;
 
 int main()
 {
-    RenderWindow window(VideoMode(HIGHT, LENGHT), "Charges & Lines");
+    RenderWindow window(VideoMode(LENGHT, HIGHT), "Charges & Lines");
 
     std::vector<Vertex> forward_points;
     std::vector<Vertex> backward_points;
@@ -12,8 +12,7 @@ int main()
     std::vector<CircleShape> positive_charges;
     std::vector<CircleShape> negative_charges;
 
-    std::vector<CircleShape> charges1;
-    std::vector<CircleShape> charges2;
+    std::vector<CircleShape> LineBalls;
 
     std::vector<Vertex> centres_positive;
     std::vector<Vertex> centres_negative;
@@ -49,34 +48,20 @@ int main()
                     if(event.mouseButton.button  == Mouse::Left)
                     {
                         CalculateForce(window, forward_points, backward_points, centres_negative, centres_positive);
-                        sf::CircleShape circle(POINT_RADIUS);
-                        circle.setFillColor(sf::Color::White);
-                        circle.setPosition(forward_points[0].position.x , forward_points[0].position.y);
-                        charges1.push_back(circle);
-
-                        for(int i = 1; i < forward_points.size(); i ++)
-                        {
-                            sf::CircleShape circle2(POINT_RADIUS);
-                            circle2.setFillColor(sf::Color::Green);
-                            circle2.setPosition(forward_points[i].position.x , forward_points[i].position.y);
-                            charges2.push_back(circle2);
-                        }
+                        CreateLine(LineBalls, forward_points, backward_points);
                     }
                     break;
             }
         }
         window.clear();
 
+        for(auto it : LineBalls)
+            window.draw(it);
         for(auto it : positive_charges)
             window.draw(it);
         for(auto it : negative_charges)
             window.draw(it);
 
-        for(auto it : charges1)
-            window.draw(it);
-        for(auto it : charges2)
-            window.draw(it);
-        
         window.display();
     }
 
